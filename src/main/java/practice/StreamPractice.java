055563f4da3569c57b5a3787e8abfd1cf83c8da7
Dima <nds081989@hmail.com>
2025-09-01 12:26:15 -0400

    Fixed issues with order of imports

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.function.Predicate;
 import model.Candidate;
 import model.Cat;
 import model.Person;
