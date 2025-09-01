fd58b2db1996fa04e9945d0fb37a4b9b5ac2896c
Dima <nds081989@hmail.com>
2025-09-01 12:00:02 -0400

    Implement all methods in class StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate c) {
+        String[] years = c.getPeriodsInUkr().split("-");
+        int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+
+        return c.getAge() >= 35
+                && c.isAllowedToVote()
+                && c.getNationality().equals("Ukrainian")
+                && yearsInUkraine >= 10;
+    }
 }
