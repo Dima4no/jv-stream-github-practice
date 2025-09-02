20242e42cc9adbaf15f429dd3c83a4b76f6cc7d5
Dima <nds081989@hmail.com>
2025-09-01 20:44:46 -0400

    Fixed all issues

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,15 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int REQUIRED_YEARS = 10;
+    private static final String REQUIRED_CITIZENSHIP = "Ukrainian";
 
     @Override
     public boolean test(Candidate c) {
         String[] years = c.getPeriodsInUkr().split("-");
         int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
 
-        return c.getAge() >= 35
+        return c.getAge() >= MIN_AGE
                 && c.isAllowedToVote()
-                && c.getNationality().equals("Ukrainian")
-                && yearsInUkraine >= 10;
+                && c.getNationality().equals(REQUIRED_CITIZENSHIP)
+                && yearsInUkraine >= REQUIRED_YEARS;
     }
 }
